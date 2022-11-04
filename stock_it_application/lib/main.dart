60e2fed212fe8b313547c07d106e63c6657e10a7
GamingGuitarMan <aty2017@yahoo.com>
2022-11-04 14:09:18 -0400

    Adding getX dependencies and package import

--- stock_it_application/lib/main.dart
+++ stock_it_application/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
