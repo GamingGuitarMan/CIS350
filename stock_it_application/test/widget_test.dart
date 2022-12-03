b8a04f0c77f1641f3b24cb4c9c5d8b9c07a18f39
Mohamed <71032586+EGR226MohamedAbdirahman@users.noreply.github.com>
2022-12-02 19:15:10 -0500

    Update widget_test.dart

--- stock_it_application/test/widget_test.dart
+++ stock_it_application/test/widget_test.dart
@@ -29,3 +29,48 @@ void main() {
   });
 }
 */
+
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/route_manager.dart';
+import 'dart:async';
+
+import 'package:stock_it_application/app/home/Binding/controller_binding.dart';
+import 'package:stock_it_application/app/home/Binding/homeBinding.dart';
+import 'package:stock_it_application/app/home/Controller/homeController.dart';
+import 'package:stock_it_application/app/home/Views/homepage.dart';
+import 'package:stock_it_application/main.dart';
+import 'package:stock_it_application/app/home/Views/customFullScrenDialog.dart';
+import 'package:stock_it_application/app/home/Controller/homeController.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  test(HomeController(), () {
+    Firebase.initializeApp();
+
+    final saveUpdateTest = HomeController().saveUpdateItem;
+
+    saveUpdateTest("Eggs", "4", "2", 2); // error test
+
+    expect(saveUpdateTest, "Eggs , 4, 1, 2");
+  });
+}
+
+// Test Save/Update
+// Database: For Item that doesnt exist
+// Validate name
+// delete data
+// time update
+// Decrement date life
+// Home controller
