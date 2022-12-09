96883401add88c0a698de5f35db157385dc1c0b5
GamingGuitarMan <aty2017@yahoo.com>
2022-12-09 13:07:20 -0500

    Comments on code

--- stock_it_application/lib/app/home/Controller/homeController.dart
+++ stock_it_application/lib/app/home/Controller/homeController.dart
@@ -7,29 +7,45 @@ import '../Views/customSnackBar.dart';
 import '../itemModel.dart';
 import '../timeModel.dart';
 
+/*
+ * HomeController class is the main control class
+ * used to handle the logic of items, the changing of their attributes,
+ * and the scheduling of shopping appointments for the user
+ */
 class HomeController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   late TextEditingController nameController, shelfLifeController;
 
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
+  // creating references for collection references in database
   late CollectionReference collectionReference;
   late CollectionReference timeReference;
 
+  // create a RxList of ItemModel objects
   RxList<ItemModel> items = RxList<ItemModel>([]);
 
+  // function for initialization of the app
   @override
   void onInit() {
     super.onInit();
     nameController = TextEditingController();
     shelfLifeController = TextEditingController();
+    // creates reference to 'items' collection in database
     collectionReference = firebaseFirestore.collection('items');
+    // gets list of items
     items.bindStream(getAllItems());
+    // calls timeUpdate() to update shelfLife of items
+    timeUpdate();
+    // if app left open, checks every 3 hours if a day has passed to call timeUpdate
     Timer threeHourTimer = Timer.periodic(Duration(hours: 3), (timer) {
       timeUpdate();
     });
   }
 
+  /*
+   * checks that the name of an item is not empty
+   */
   String? validateName(String value) {
     if (value.isEmpty) {
       return "Name cannot be empty silly goose";
@@ -37,6 +53,9 @@ class HomeController extends GetxController {
     return null;
   }
 
+  /*
+   * Checks that the shelfLife is not empty
+   */
   String? validateShelfLife(String value) {
     if (value.isEmpty) {
       return "shelf life cannot be empty";
@@ -44,6 +63,11 @@ class HomeController extends GetxController {
     return null;
   }
 
+  /*
+   * This function saves or updates an item
+   * Takes Strings name, shelfLife, docId, and int addEditFlag as parameters
+   * addEditFlag of 1 for add, 2 for edit 
+   */
   void saveUpdateItem(
       String name, String shelfLife, String docId, int addEditFlag) {
     final isValid = formKey.currentState!.validate();
@@ -51,18 +75,23 @@ class HomeController extends GetxController {
       return;
     }
     formKey.currentState!.save();
+
+    // checks if addEditFlag is 1 for add
     if (addEditFlag == 1) {
       CustomFullScreenDialog.showDialog();
+      // adding new item to database
       collectionReference
           .add({'name': name, 'shelfLife': shelfLife}).whenComplete(() {
         CustomFullScreenDialog.cancelDialog();
         clearEditingControllers();
         Get.back();
+        // showing confimation if item added
         CustomSnackBar.showSnackBar(
             context: Get.context,
             title: "Item Added",
             message: "Item added successfully",
             backgroudColor: Color.fromARGB(255, 116, 163, 118));
+        // showing error if item failed to add
       }).catchError((error) {
         CustomFullScreenDialog.cancelDialog();
         CustomSnackBar.showSnackBar(
@@ -71,19 +100,23 @@ class HomeController extends GetxController {
             message: "Something went wrong",
             backgroudColor: Colors.red);
       });
+      // checks if addEditFlag is 2 for updating item
     } else if (addEditFlag == 2) {
       CustomFullScreenDialog.showDialog();
+      // getting and updating the item with new name and/or shelfLife
       collectionReference
           .doc(docId)
           .update({'name': name, 'shelfLife': shelfLife}).whenComplete(() {
         CustomFullScreenDialog.cancelDialog();
         clearEditingControllers();
         Get.back();
+        // showing confirmation that the item was updated
         CustomSnackBar.showSnackBar(
             context: Get.context,
             title: "Item updated",
             message: "Item updated successfully",
             backgroudColor: Color.fromARGB(255, 109, 162, 111));
+        // showing error if item failed to update
       }).catchError((error) {
         CustomFullScreenDialog.cancelDialog();
         CustomSnackBar.showSnackBar(
@@ -111,9 +144,16 @@ class HomeController extends GetxController {
     shelfLifeController.clear();
   }
 
+  /*
+   * This function gets items from the reference and returns a list of items 
+   */
   Stream<List<ItemModel>> getAllItems() => collectionReference.snapshots().map(
       (query) => query.docs.map((item) => ItemModel.fromMap(item)).toList());
 
+  /*
+   *  This function deletes an item from the database
+   *  Takes String docId as parameter 
+   */
   void deleteData(String docId) {
     CustomFullScreenDialog.showDialog();
     collectionReference.doc(docId).delete().whenComplete(() {
@@ -134,19 +174,23 @@ class HomeController extends GetxController {
     });
   }
 
+  /*
+   * This function checks if a day has passed in order to decrement the shelf life of items
+   */
   void timeUpdate() async {
+    // getting the data in colleciton
     final docRef = firebaseFirestore.collection('time').doc('1');
     docRef.get().then(
       (DocumentSnapshot doc) {
         final data = doc.data() as Map<String, dynamic>;
         final currentTime = Timestamp.fromMillisecondsSinceEpoch(
             DateTime.now().millisecondsSinceEpoch);
-
         var lastTimestamp;
         // getting timpstamp from "lastTime" key
         data.forEach((key, value) {
           lastTimestamp = value;
         });
+
         // getting day difference from the current time and last time in database
         var dayDiff = currentTime
             .toDate()
@@ -163,14 +207,23 @@ class HomeController extends GetxController {
     );
   }
 
+  /*
+   * This function updates the shopping time selected by user 
+   */
   shopTime(DateTime newTime) {
     firebaseFirestore.collection('time').doc('2').update({'shop': newTime});
   }
 
+  /*
+   *  This function decrements the shelf life of all items 
+   */
   void decrementShelfLife(int dayDiff) {
+    // loops through all items in list
     for (var item in items) {
       int tempShelfLife = int.parse(item.shelfLife as String);
+      // subtracts the number of days passed from shelfLife
       tempShelfLife -= dayDiff;
+      // updates items with new shelf life
       collectionReference
           .doc(item.docId)
           .update({'name': item.name, 'shelfLife': tempShelfLife.toString()});
--- stock_it_application/lib/main.dart
+++ stock_it_application/lib/main.dart
@@ -7,6 +7,9 @@ import 'package:stock_it_application/app/home/Binding/controller_binding.dart';
 import 'package:stock_it_application/app/home/Binding/homeBinding.dart';
 import 'package:stock_it_application/app/home/Views/homepage.dart';
 
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+
 import 'app/routes/app_pages.dart';
 
 void main() async {
@@ -39,3 +42,101 @@ void main() async {
     ),
   );
 }
+
+
+
+
+// OPEN AI FUNCTION FOR NOTIFICATIONS AND FOR SCHEDULING
+// TODO: EDIT AND IMPLEMENT WITH CURRENT APP
+/**
+ import 'package:flutter/material.dart';
+import 'package:firebase/firebase.dart' as firebase;
+import 'package:firebase/firestore.dart' as fs;
+import 'package:firebase/messaging.dart' as fcm;
+
+void main() {
+  runApp(MyApp());
+
+  // Initialize Firebase and Firestore
+  firebase.initializeApp();
+  fs.firestore().settings({
+    'timestampsInSnapshots': true,
+  });
+
+  // Request permission to show notifications and handle incoming messages
+  fcm.Messaging.onMessage((message) {
+    showNotification(message);
+  });
+  fcm.Messaging.requestPermission();
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime _selectedDate;
+  TimeOfDay _selectedTime;
+  String _selectedFrequency;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            RaisedButton(
+              onPressed: () async {
+                // Show the date picker and wait for the user to select a date
+                final DateTime picked = await showDatePicker(
+                  context: context,
+                  initialDate: _selectedDate,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2030),
+                );
+
+                // If a date was selected, update the state
+                if (picked != null && picked != _selectedDate)
+                  setState(() {
+                    _selectedDate = picked;
+                  });
+              },
+              child: Text('Select a date'),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                // Show the time picker and wait for the user to select a time
+                final TimeOfDay picked = await showTimePicker(
+                  context: context,
+                  initialTime: _selectedTime,
+                );
+
+                // If a time was selected, update the state
+                if (picked != null && picked != _selectedTime)
+                  setState(() {
+                    _selectedTime = picked;
+                  });
+              },
+              child: Text('Select a time'),
+            ),
+            DropdownButton<String>(
+              value: _selectedFrequency,
+              onChanged: (String newValue) {
+                setState(() {
+                  _selectedFrequency = newValue;
+                });
+              },
+              items: <String>['Daily', 'Weekly', 'Monthly']
+                .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+            ),
+            RaisedButton(
+              onPressed: () {
+                // Create a new appointment with the selected date, time, and frequency
+                final appointment = firebase.firestore().collection('appointments').
+ */
